b05912e26e859a4053610bde188a1ef46b044ef7
Aniket Muruskar <aniket23muruskar@gmail.com>
2017-03-11 17:01:08 +0530

    update for post,comment,ng-tasty pagination added

--- app/models/post/comment.js
+++ app/models/post/comment.js
@@ -1,23 +1,13 @@
 var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema({
-  name: { 
-	   type: String,
-	   default: ''
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    unique: true
-  },
-  hash: String,
-  salt: String,
-  admin:{ 
-     type: Boolean,
-     default: false
-  },
-  status:{ 
-	   type: Boolean,
-	   default: false
-  } 
+  body: String,
+  author: String,
+  upvotes: {type: Number, default: 0},
+  post: { 
+  	type: mongoose.Schema.Types.ObjectId,
+  	ref: 'Post'
+  }
 });
+
+mongoose.model('Comment', CommentSchema);
