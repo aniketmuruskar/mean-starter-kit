a15a2cd633da693db063b80956870282d4c30853
Aniket Muruskar <aniket23muruskar@gmail.com>
2017-03-31 19:25:35 +0530

    updated

--- public/app/shared/services/http.interceptor.factory.js
+++ public/app/shared/services/http.interceptor.factory.js
@@ -5,12 +5,15 @@ angular.module('shared.module')
 		.factory('myHttpInterceptor', myHttpInterceptor);
 
 	/*@ngInject*/
-	myHttpInterceptor.$inject = ['$q', 'auth'];
-	function myHttpInterceptor($q, auth) {
+	myHttpInterceptor.$inject = ['$q', '$injector'];
+	function myHttpInterceptor($q, $injector) {
 	    return {
 	        request: function(config) {
+	        	//injected manually to get around circular dependency problem.
+                var AuthService = $injector.get('auth');
+
 	        	config.headers = config.headers || {};
-                var token = auth.getToken();
+                var token = AuthService.getToken();
                 alert(token);
                 if (token) {
                     config.headers.Authorization = 'Bearer ' + token;
@@ -34,27 +37,3 @@ angular.module('shared.module')
 	    };
 	};
 })();
-
-/**
-function myHttpInterceptor($q){
-    return {
-        request: function(config) {
-            return config;
-        },
-
-        responseError: function (response) {
-            alert("Please login again.");
-            if (response.status === 401) {
-               window.location.href = "http://localhost:8886/#!/auth/sign-in";
-            }
-            if (response.status == 403) {
-                alert("Permission denied.");
-            }
-            if (response.status == 400) {
-                alert("Exception occured. Bad request.");
-            }
-            return $q.reject(response);
-        }
-    };
-};
-*/
